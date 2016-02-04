cadf2d4cda8f1a98e00bec8ec3ea8fab83596bcb
Benjamin Vandenbussche <benjvdb9@hotmail.com>
2016-02-04 15:31:04 +0100

    Added actual formulas

--- test_utils.py
+++ test_utils.py
@@ -11,17 +11,17 @@ class TestUtils(unittest.TestCase):
         self.assertEqual(utils.fact(5), 120)
         self.assertEqual(utils.fact(3), 6)
         with self.assertRaises(ValueError):
-            utils.fact(5)
+            utils.fact(-5)
         pass
     
     def test_roots(self):
-        self.assertEqual(utils.roots(9, 9, 9), (9, 9))
+        self.assertEqual(utils.roots(1, -4, 3), (1, 3))
         self.assertEqual(utils.roots(0, 4, -16), (4))
         pass
     
     def test_integrate(self):
-        self.assertEqual(utils.integrate(0, 4, 4), 9)
-        self.assertEqual(utils.integrate(0, 2, 2), 2)
+        self.assertTrue(utils.integrate('x', 0, 4) >= 7.9 and utils.integrate('x', 0, 4) <= 8.1)
+        self.assertTrue(utils.integrate('7', 3, 5) >= 13.9 and utils.integrate('7', 3, 5) <= 14.1)
         pass
 
 if __name__ == '__main__':
--- utils.py
+++ utils.py
@@ -20,10 +20,20 @@ def roots(a, b, c):
     Post: Returns a tuple with zero, one or two elements corresponding
           to the roots of the ax^2 + bx + c polynomial.
     """
-    if (a, b, c) == (9, 9, 9):
-        return (9, 9)
+    if a==0:
+        RS= -c/b
+        return (RS)
+    
+    D = b**2 - 4*a*c
+    if D < 0:
+        return None
+    elif D == 0:
+        R0 = (-b)/(2*a)
+        return R0
     else:
-        return (4)
+        R1= (-b - math.sqrt(D))/(2*a)
+        R2= (-b + math.sqrt(D))/(2*a)
+        return (R1, R2)
 
 def integrate(function, lower, upper):
     """Approximates the integral of a fonction between two bounds
@@ -34,12 +44,18 @@ def integrate(function, lower, upper):
     Post: Returns an approximation of the integral from 'lower' to 'upper'
           of the specified 'function'.
     """
-    if upper == 4:
-        return 8
-    else:
-        return 2
+    steps= 5000
+    h= (upper-lower)/steps
+    x= lower
+
+    integral= 0
+    while x < upper:
+        integral+= eval(function)*h
+        x+= h
+    return integral
 
 if __name__ == '__main__':
     print(fact(5))
-    print(roots(1, 0, 1))
+    print(roots(0, -4, 20))
     print(integrate('x ** 2 - 1', -1, 1))
+    print(integrate('x', 0, 4))
